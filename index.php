f90dc2f0d7d7627d1081aee74237acf8a757cfa6
TaboadaGR <taboadarodriguezgr@gmail.com>
2022-02-10 16:15:53 +0100

    version1.1

--- index.php
+++ index.php
@@ -6,10 +6,16 @@ $update = json_decode(file_get_contents("php://input"), TRUE)
 $chatId = $update["message"]["chat"]["id"];
 $message = $update["message"]["text"];
 
-if (strpos($message, "/weather") === 0) {
-$location = substr($message, 9);
-$weather = json_decode(file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=".$location."&appid=mytoken"), TRUE)["weather"][0]["main"];
-file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=Here's the weather in ".$location.": ". $weather);
+if (strpos($message, "/weather") === 0{
+    $url=$path."/sendMessage?chat_id=".$chatId."&parse_mode=HTML&text=hola";
+    file_get_contents($url);
 }
+
+/*if (strpos($message, "/weather") === 0) {
+    $location = substr($message, 9);
+    $weather = json_decode(file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=".$location."&appid=mytoken"), TRUE)["weather"][0]["main"];
+    file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=Here's the weather in ".$location.": ". $weather);
+    }*/
+
 
 ?>
