19316f5dd9589c75c86c5893fe8e169cabe81583
TaboadaGR <taboadarodriguezgr@gmail.com>
2022-02-14 17:45:22 +0100

    version3.3

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <?php
 
 $path = "https://api.telegram.org/bot"
-$apikeytelegram="5198652264:AAHjes9nmynBblfzw7TZ4bRYwM_po-aF1Lg";
-$update = json_decode(file_get_contents("php://input"), TRUE);
+// $apikeytelegram="5198652264:AAHjes9nmynBblfzw7TZ4bRYwM_po-aF1Lg";
+// $update = json_decode(file_get_contents("php://input"),TRUE);
 
 
-$chatId = $update["message"]["chat"]["id"];
-$message = $update["message"]["text"];
+// $chatId = $update["message"]["chat"]["id"];
+// $message = $update["message"]["text"];
 
-if (strpos($message, "/weather") === 0) {
-$location = substr($message, 9);
-$weather = json_decode(file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=".$location."&appid=cb5bdb19e2f810101fb82c512a2ab64a"), TRUE)["weather"][0]["main"];
-file_get_contents($path.$apikeytelegram"/sendmessage?chat_id=".$chatId."&text=Here's the weather in ".$location.": ". $weather);
-}
+// if (strpos($message, "/weather") === 0) {
+// $location = substr($message, 9);
+// $weather = json_decode(file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=".$location."&appid=cb5bdb19e2f810101fb82c512a2ab64a"), TRUE)["weather"][0]["main"];
+// file_get_contents($path.$apikeytelegram"/sendmessage?chat_id=".$chatId."&text=Here's the weather in ".$location.": ". $weather);
+// }
 
 ?>
