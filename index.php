6ca6d9f8e422d5b8d748da018f9494d019a53569
TaboadaGR <taboadarodriguezgr@gmail.com>
2022-02-14 19:59:55 +0100

    version4.5

--- index.php
+++ index.php
@@ -9,9 +9,29 @@ $update = json_decode(file_get_contents("php://input"),TRUE);
  $message = $update["message"]["text"];
 
  if (strpos($message, "/weather") === 0) {
- $location = substr($message, 9);
- $weather = json_decode(file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=".$location."&appid=cb5bdb19e2f810101fb82c512a2ab64a"), TRUE)["weather"][0]["main"];
-file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=Here's the weather in ".$location.": ". $weather);
- }
+    $location = substr($message, 9);
+    $weather = json_decode(file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=".$location."&appid=cb5bdb19e2f810101fb82c512a2ab64a"), TRUE)["weather"][0]["main"];
+    file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=Here's the weather in ".$location.": ". $weather);
+}
 
+if(strpos($message, "/covid") === 0){
+    $client = new http\Client;
+$request = new http\Client\Request;
+
+$request->setRequestUrl('https://covid-19-data.p.rapidapi.com/country/code');
+$request->setRequestMethod('GET');
+$request->setQuery(new http\QueryString([
+	'code' => 'it'
+]));
+
+$request->setHeaders([
+	'x-rapidapi-host' => 'covid-19-data.p.rapidapi.com',
+	'x-rapidapi-key' => '6d433c5dcbmsh8f04c3c9f812e94p1f26d6jsn166e00f9d911'
+]);
+
+$client->enqueue($request)->send();
+$response = $client->getResponse();
+
+echo $response->getBody();
+}
 ?>
