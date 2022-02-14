5cf02a14e1be82edc6db4b009047ed899cc4655c
TaboadaGR <taboadarodriguezgr@gmail.com>
2022-02-14 17:51:34 +0100

    version3.9

--- index.php
+++ index.php
@@ -8,10 +8,10 @@ $update = json_decode(file_get_contents("php://input"),TRUE);
  $chatId = $update["message"]["chat"]["id"];
  $message = $update["message"]["text"];
 
-// if (strpos($message, "/weather") === 0) {
+ if (strpos($message, "/weather") === 0) {
 // $location = substr($message, 9);
 // $weather = json_decode(file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=".$location."&appid=cb5bdb19e2f810101fb82c512a2ab64a"), TRUE)["weather"][0]["main"];
 // file_get_contents($path.$apikeytelegram"/sendmessage?chat_id=".$chatId."&text=Here's the weather in ".$location.": ". $weather);
-// }
+ }
 
 ?>
