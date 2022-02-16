8c5ff0078808ded9921aab59222e13a8e264b38e
TaboadaGR <taboadarodriguezgr@gmail.com>
2022-02-16 16:04:16 +0100

    version 5.2

--- index.php
+++ index.php
@@ -8,26 +8,26 @@ $update = json_decode(file_get_contents("php://input"),TRUE);
  $chatId = $update["message"]["chat"]["id"];
  $message = $update["message"]["text"];
 
- if (strpos($message, "/weather") === 0) {
-    $location = substr($message, 9);
-    $weather = json_decode(file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=".$location."&appid=cb5bdb19e2f810101fb82c512a2ab64a"), TRUE)["weather"][0]["main"];
-    file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=El tiempo en ".$location." está: ". $weather);
-}
+ $reply = $update["message"]["reply_to_message"]["text"];
 
-if(strpos($message, "/covid") === 0){
-    $client = new http\Client;
-    $request = new http\Client\Request;
-    
-    $request->setRequestUrl('https://covid-193.p.rapidapi.com/countries');
-    $request->setRequestMethod('GET');
-    $request->setHeaders([
-        'x-rapidapi-host' => 'covid-193.p.rapidapi.com',
-        'x-rapidapi-key' => '6d433c5dcbmsh8f04c3c9f812e94p1f26d6jsn166e00f9d911'
-    ]);
-    
-    $client->enqueue($request)->send();
-    $response = $client->getResponse();
-    
-    echo $response->getBody();
+ if(isset($reply)){}
+ 
+ function sendMessage($chatId, $message, $repl){
+
+     if($repl == TRUE) {
+        $reply_mark = array(array('force_reply' => TRUE);
+        $url = $GLOBALS[website].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&reply_markup='.json_encode($reply_mark).'&text='.urlencode($response);
+     }
+     else $url = $GLOBALS[website].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($response);
+
+     file_get_contents($url);
+ }
+
+
+ if (strpos($message, "/weather") == 0) {
+    $location = substr($message, 9);
+    $weather = json_decode(file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=".$location."&appid=cb5bdb19e2f810101fb82c512a2ab64a"), TRUE);
+    $weather = $weather["weather"][0]["main"];
+    file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=El tiempo en ".$location." está: ". $weather);
 }
 ?>
