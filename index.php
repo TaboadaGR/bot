78dbe9fe9aefe85f5b11e84762ac54b6799e5a78
TaboadaGR <taboadarodriguezgr@gmail.com>
2022-02-16 18:08:47 +0100

    version 6.1

--- index.php
+++ index.php
@@ -8,9 +8,9 @@ $update = json_decode(file_get_contents("php://input"),TRUE);
  $chatId = $update["message"]["chat"]["id"];
  $message = $update["message"]["text"];
 
-//  $reply = $update["message"]["reply_to_message"]["text"];
+  $reply = $update["message"]["reply_to_message"]["text"];
 
-//  if(isset($reply)){
+  if(isset($reply)){
 
     // function sendMessage($chatId, $message, $repl){
 
@@ -21,7 +21,6 @@ $update = json_decode(file_get_contents("php://input"),TRUE);
     //     else $url = $GLOBALS[website].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($response);
    
     //     file_get_contents($url);
-    // }
    
    
     if (strpos($message, "/weather") === 0) {
@@ -30,7 +29,7 @@ $update = json_decode(file_get_contents("php://input"),TRUE);
        $weather = json_decode(file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=".$location."&appid=cb5bdb19e2f810101fb82c512a2ab64a"), TRUE)["weather"][0]["main"];
        file_get_contents($path."/sendmessage?chat_id=".$chatId.'&parse_mode=HTML&reply_markup='.json_encode($reply_mark)."&text=Here's the weather in ".$location.": ". $weather);
    }
-//  }
+ }
  
  
 ?>
