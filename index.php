5858577b1f7c2c9056e2d658b3d2817cfc9c0ae0
TaboadaGR <taboadarodriguezgr@gmail.com>
2022-02-16 18:40:55 +0100

    version 6.4

--- index.php
+++ index.php
@@ -30,6 +30,9 @@ $update = json_decode(file_get_contents("php://input"),TRUE);
        $weather = json_decode(file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=".$location."&appid=cb5bdb19e2f810101fb82c512a2ab64a"), TRUE)["weather"][0]["main"];
        file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=Here's the weather in ".$location.": ". $weather);
    }
+   else {
+    file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=no te entiendo ");
+   }
 //  }
  
  
