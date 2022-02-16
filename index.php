0293c791db8bc292acb164bf3b2a67a654923286
TaboadaGR <taboadarodriguezgr@gmail.com>
2022-02-16 16:28:47 +0100

    version 5.4

--- index.php
+++ index.php
@@ -24,17 +24,34 @@ $update = json_decode(file_get_contents("php://input"),TRUE);
  }
 
 
- if (strpos($message, "/weather") == 0) {
+ if (strpos($message, "/weather") === 0) {
     $location = substr($message, 9);
-    $weather = json_decode(file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=".$location."&appid=cb5bdb19e2f810101fb82c512a2ab64a"), TRUE);
-    $weather = $weather["weather"][0]["main"];
-    file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=El tiempo en ".$location." está: ". $weather);
+$weather = json_decode(file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=".$location."&appid=cb5bdb19e2f810101fb82c512a2ab64a"), TRUE)["weather"][0]["main"];
+file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=Here's the weather in ".$location.": ". $weather);
 }
 
+
+
 if (strpos($message, "/covid") == 0) {
-    //$location = substr($message, 9);
-    $weather = json_decode(file_get_contents("https://api.covidtracking.com/v1/us/daily.json"), TRUE);
-    $weather = $weather["weather"][0]["main"];
-    file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=El tiempo en ".$location." está: ". $weather);
+    require_once 'HTTP/Request2.php';
+    $request = new HTTP_Request2();
+    $request->setUrl('https://covidtracking.com/api/states');
+    $request->setMethod(HTTP_Request2::METHOD_GET);
+    $request->setConfig(array(
+      'follow_redirects' => TRUE
+    ));
+    try {
+      $response = $request->send();
+      if ($response->getStatus() == 200) {
+        echo $response->getBody();
+      }
+      else {
+        echo 'Unexpected HTTP status: ' . $response->getStatus() . ' ' .
+        $response->getReasonPhrase();
+      }
+    }
+    catch(HTTP_Request2_Exception $e) {
+      echo 'Error: ' . $e->getMessage();
+    }
 }
 ?>
