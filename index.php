a627a0dd4b311e3e7d0a70df00384a82e0e6b0cf
TaboadaGR <taboadarodriguezgr@gmail.com>
2022-02-17 15:57:18 +0100

    version 6.7

--- index.php
+++ index.php
@@ -8,31 +8,33 @@ $update = json_decode(file_get_contents("php://input"),TRUE);
  $chatId = $update["message"]["chat"]["id"];
  $message = $update["message"]["text"];
 
-//   $reply = $update["message"]["reply_to_message"]["text"];
+  $reply = $update["message"]["reply_to_message"]["text"];
 
-//   if(isset($reply)){
+  if(isset($reply)){
 
-    // function sendMessage($chatId, $message, $repl){
+    sendMessage($chatId,$message,TRUE);
 
-    //     if($repl == TRUE) {
-    //        $reply_mark = array(array('force_reply' => TRUE);
-    //        $url = $GLOBALS[website].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&reply_markup='.json_encode($reply_mark).'&text='.urlencode($response);
-    //     }
-    //     else $url = $GLOBALS[website].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($response);
+    function sendMessage($chatId, $message, $repl){
+
+        if($repl == TRUE) {
+           $reply_mark = array(array('force_reply' => TRUE);
+           $url = $path.'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&reply_markup='.json_encode($reply_mark).'&text='.urlencode($message);
+        }
+        else $url = $path.'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($message);
    
-    //     file_get_contents($url);
+        file_get_contents($url);
    
-//  }
+ }
    
-    if (strpos($message, "/tiempo") === 0) {
-        // $reply_mark = array(array('force_reply' => TRUE);
-       $location = substr($message, 9);
-       $weather = json_decode(file_get_contents("https://www.el-tiempo.net/api/json/v2/home".$location."&appid=cb5bdb19e2f810101fb82c512a2ab64a"), TRUE)["weather"][0]["main"];
-       file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=El tiempo en ".$location."es: ". $weather);
-   }
-   else {
-    file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=no te entiendo ");
-   }
+//     if (strpos($message, "/tiempo") === 0) {
+//         // $reply_mark = array(array('force_reply' => TRUE);
+//        $location = substr($message, 9);
+//        $weather = json_decode(file_get_contents("https://www.el-tiempo.net/api/json/v2/home".$location."&appid=cb5bdb19e2f810101fb82c512a2ab64a"), TRUE)["weather"][0]["main"];
+//        file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=El tiempo en ".$location."es: ". $weather);
+//    }
+//    else {
+//     file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=no te entiendo ");
+//    }
 
  
  
