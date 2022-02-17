61792005d334a2dbf3ed5a1d96f469b6f8328591
TaboadaGR <taboadarodriguezgr@gmail.com>
2022-02-17 16:43:09 +0100

    version 6.8

--- index.php
+++ index.php
@@ -9,22 +9,38 @@ $update = json_decode(file_get_contents("php://input"),TRUE);
  $message = $update["message"]["text"];
 
   $reply = $update["message"]["reply_to_message"]["text"];
+  $replya = explode(" ",$reply);
 
-  if(isset($reply)){
 
-    sendMessage($chatId,$message,TRUE);
+  function sendMessage($chatId, $message, $repl){
 
-    function sendMessage($chatId, $message, $repl){
+    if($repl == TRUE) {
+       $reply_mark = array(array('force_reply' => TRUE);
+       $url = $path.'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&reply_markup='.json_encode($reply_mark).'&text='.urlencode($message);
+    }
+    else $url = $path.'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($message);
 
-        if($repl == TRUE) {
-           $reply_mark = array(array('force_reply' => TRUE);
-           $url = $path.'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&reply_markup='.json_encode($reply_mark).'&text='.urlencode($message);
-        }
-        else $url = $path.'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($message);
-   
-        file_get_contents($url);
-   
- }
+    file_get_contents($url);
+}
+
+  if(empty($reply)){
+
+    switch($message){
+        case "Hola":
+             $response = "Hola que tal?";
+            sendMessage($chatId,$response,FALSE);
+            break;
+
+        case default:
+            $response = "No te he entendido";
+            sendMessage($chatId,$response,FALSE);
+            break;
+
+  }
+  else {
+      $response = "¿Qué quieres saber?";
+      sendMessage($chatId,$response,TRUE);
+  }
    
 //     if (strpos($message, "/tiempo") === 0) {
 //         // $reply_mark = array(array('force_reply' => TRUE);
