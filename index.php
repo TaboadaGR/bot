6402322d4e594a16976765ca8b328b1483f1e501
TaboadaGR <taboadarodriguezgr@gmail.com>
2022-02-17 17:04:43 +0100

    version 6.9

--- index.php
+++ index.php
@@ -3,25 +3,26 @@
 echo "hola";
 $apikeytelegram="5198652264:AAHjes9nmynBblfzw7TZ4bRYwM_po-aF1Lg";
 $path = "https://api.telegram.org/bot".$apikeytelegram;
-$update = json_decode(file_get_contents("php://input"),TRUE);
+$update = file_get_contents("php://input");
+$update = json_decode($update,TRUE);
 
 
  $chatId = $update["message"]["chat"]["id"];
  $message = $update["message"]["text"];
 
-  $reply = $update["message"]["reply_to_message"]["text"];
-  $replya = explode(" ",$reply);
+//   $reply = $update["message"]["reply_to_message"]["text"];
+//   $replya = explode(" ",$reply);
 
 
-//   function sendMessage($chatId, $message, $repl){
+// //   function sendMessage($chatId, $message, $repl){
 
-//     if($repl == TRUE) {
-//        $reply_mark = array(array('force_reply' => TRUE);
-//        $url = $path.'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&reply_markup='.json_encode($reply_mark).'&text='.urlencode($message);
-//     }
-//     else $url = $path.'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($message);
+// //     if($repl == TRUE) {
+// //        $reply_mark = array(array('force_reply' => TRUE);
+// //        $url = $path.'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&reply_markup='.json_encode($reply_mark).'&text='.urlencode($message);
+// //     }
+// //     else $url = $path.'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($message);
 
-//     file_get_contents($url);
+// //     file_get_contents($url);
 }
 
   if(empty($reply)){
