f4c6224f6bd56daa224536a29d032a5941526851
TaboadaGR <taboadarodriguezgr@gmail.com>
2022-02-21 16:51:07 +0100

    version 7.0

--- index.php
+++ index.php
@@ -19,26 +19,27 @@ $replya = explode(" ",$reply);
 
 /*Función para enviar un mensaje al usuario, en función
 de lo que envie al bot.*/ 
-function sendMessage($chatId, $message, $repl){
+function sendMessage($chatId, $message,$repl){
 
 
     if($repl == TRUE) {
        $reply_mark = array('force_reply' => TRUE);
        $url = $path.'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&reply_markup='.json_encode($reply_mark).'&text='.urlencode($message);
     }
-    else 
-    $url = $path.'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($message);
+    else{
+        $url = $path.'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($message);
+    }
 
     file_get_contents($url);
 }
 
-  if(empty($reply)){
+if(empty($reply)){
 
 //Comprueba con el "switch" que ha introducido el usuario
     switch($command[0]){
         case "Hola":
             $response = "Hola que tal?";
-            sendMessage($chatId,$response,TRUE);
+            sendMessage($chatId,$response,FALSE);
             break;
 
         case "/tiempo":
@@ -53,10 +54,10 @@ function sendMessage($chatId, $message, $repl){
 
   }
 }
-  else {
-      switch($replya[0]){
-        case "¿Qué":
-            getTiempo($chatId,$lugar);
+else {
+    switch($replya[0]){
+        case "¿De":
+            getTiempo($chatId,$message);
             break;
         
         default:
@@ -64,25 +65,13 @@ function sendMessage($chatId, $message, $repl){
             sendMessage($chatId,$response,FALSE);
             break;
             
-      }
+    }
+}
+
+  function getTiempo($chatId,$lugar){
+
+    sendMessage($chatId,$lugar,FALSE);
   }
-
-//   function getTiempo($chatId,$lugar){
-
-//     sendMessage($chatId,$lugar,FALSE);
-//   }
-   
-//     if (strpos($message, "/tiempo") === 0) {
-//         // $reply_mark = array(array('force_reply' => TRUE);
-//        $location = substr($message, 9);
-//        $weather = json_decode(file_get_contents("https://www.el-tiempo.net/api/json/v2/home".$location."&appid=cb5bdb19e2f810101fb82c512a2ab64a"), TRUE)
-//          $weather= ["ciudades"][1]["temperatures"]["max"];
-//        sendMessage($chatId,$weather,FALSE);
-// NOOOOOOOOOOOOOO file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=El tiempo en ".$location."es: ". $weather);
-//    }
-//    else {
-//     file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=no te entiendo ");
-//    }
 
  
  
