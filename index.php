8e31c97e2f4ff81da24444280a34b49613bcef30
TaboadaGR <taboadarodriguezgr@gmail.com>
2022-02-21 15:57:34 +0100

    version 7.0

--- index.php
+++ index.php
@@ -1,68 +1,69 @@
 <?php
 
-
+/*variables de la conexión del bot de telegrama 
+con la api de telegram*/
 $apikeytelegram="5198652264:AAHjes9nmynBblfzw7TZ4bRYwM_po-aF1Lg";
 $path = "https://api.telegram.org/bot".$apikeytelegram;
 $file = file_get_contents("php://input");
 $update = json_decode($file,TRUE);
 
-
+//variables para extraer datos del usuario 
 $chatId = $update["message"]["chat"]["id"];
 $message = $update["message"]["text"];
 $command = explode(" ",$message);
 
+//variables para contestar al usuario
+$reply = $update["message"]["reply_to_message"]["text"];
+$replya = explode(" ",$reply);
 
+/*Función para enviar un mensaje al usuario, en función
+de lo que envie al bot.*/ 
+function sendMessage($chatId, $message){
 
+    
+    if($repl == TRUE) {
+       $reply_mark = array('force_reply' => TRUE);
+       $url = $path.'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&reply_markup='.json_encode($reply_mark).'&text='.urlencode($message);
+    }
+    else 
+    $url = $path.'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($message);
 
-// $reply = $update["message"]["reply_to_message"]["text"];
-// $replya = explode(" ",$reply);
+    file_get_contents($url);
+}
 
+  if(empty($reply)){
 
-//   function sendMessage($chatId, $message){
+    switch($command[0]){
+        case "Hola":
+            $response = "Hola que tal?";
+            sendMessage($chatId,$response);
+            break;
 
-//     if($repl == TRUE) {
-//        $reply_mark = array('force_reply' => TRUE);
-//        $url = $path.'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&reply_markup='.json_encode($reply_mark).'&text='.urlencode($message);
-//     }
-//     else 
-//     $url = $path.'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($message);
+        case "/tiempo":
+            $response = "¿De qué municipio quieres consultar?";
+            sendMessage($chatId,$response);
+            break;
 
-//     file_get_contents($url);
-// }
+        default:
+            $response = "No te he entendido";
+            sendMessage($chatId,$response);
+            break;
 
-//   if(empty($reply)){
-
-//     switch($command[0]){
-//         case "Hola":
-//             $response = "Hola que tal?";
-//             sendMessage($chatId,$response);
-//             break;
-
-//         case "/tiempo":
-//             $response = "¿De qué municipio quieres consultar?";
-//             sendMessage($chatId,$response);
-//             break;
-
-//         default:
-//             $response = "No te he entendido";
-//             sendMessage($chatId,$response);
-//             break;
-
-//   }
-// }
-//   else {
-//       switch($replya[0]){
-//         case "¿Qué":
-//             getTiempo($chatId,$lugar);
-//             break;
+  }
+}
+  else {
+      switch($replya[0]){
+        case "¿Qué":
+            getTiempo($chatId,$lugar);
+            break;
         
-//         default:
-//             $response = "No te he entendido (reply)";
-//             sendMessage($chatId,$response,FALSE);
-//             break;
+        default:
+            $response = "No te he entendido (reply)";
+            sendMessage($chatId,$response,FALSE);
+            break;
             
-//       }
-//   }
+      }
+  }
 
 //   function getTiempo($chatId,$lugar){
 
